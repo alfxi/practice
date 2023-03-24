6a1f0ef1ba376a6034774759876c9bd73552a0f9
alfxi <93888045+alfxi@users.noreply.github.com>
2023-03-24 03:23:35 +0300

    1. Removed spaces between annotations and method's defines. 2. Variables containing whole numbers are now using Integer class instead of int class. 3. Comments are now written using /**...*/ for documentation function of IntelliJ IDEA

--- practice/src/main/java/ru/neoflex/practice/controller/VacationController.java
+++ practice/src/main/java/ru/neoflex/practice/controller/VacationController.java
@@ -5,16 +5,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class VacationController {
+
+    /**
+     * days - число отработанных дней, paycheck - з/п в месяц
+     */
     @GetMapping("/vacation/{days}/{paycheck}")
+    public Integer calcVacation(@PathVariable Integer days, @PathVariable Integer paycheck) {
 
-    public int calcVacation(@PathVariable int days, @PathVariable int paycheck) {
-        //a - число отработанных дней, b - з/п в месяц
-
-        int result = paycheck * 12 / days * 28;
-        // з/п умножается на 12 месяцев и делится на кол-во отработанных дней, таким образом получая
-        // средний заработок в день, который после умножается на 28 дней отпуска, положенных по закону
+        /**
+         * з.п. умножается на 12 месяцев и делится на кол-во отработанных дней, таким образом получая
+         * средний заработок в день, который после умножается на 28 дней отпуска, положенных по закону
+         */
+        Integer result = paycheck * 12 / days * 28;
 
         return result;
     }
